efaf4205cc108d0eadf5601b974712d56b3f79ba
Sebastien Besson <seb.besson@gmail.com>
2019-03-14 12:06:19 +0000

    Rename convert.sh as generate-samples.sh

---
+++ converter/generate-samples.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Script used to generate the official OME-TIFF sub-resolutions samples
+# available at 
+# https://downloads.openmicroscopy.org/images/OME-TIFF/2016-06/sub-resolutions/
+
+# Download original source files
+wget -i sample_sources.txt
+
+# Brightfield WSI
+/opt/bftools/bfconvert -no-upgrade -noflat -compression JPEG Leica-1.scn /out/Leica-1.ome.tiff
+/opt/bftools/bfconvert -no-upgrade -noflat -compression JPEG Leica-2.scn Leica-2.ome.tiff
+
+# Fluorescence WSI
+/opt/bftools/bfconvert -no-upgrade -noflat -compression LZW LuCa-7color_Scan1.qptiff /out/LuCa-7color_Scan1.ome.tiff
+
+# Z-stack
+/opt/bftools/bfconvert -no-upgrade -noflat -compression zlib retina_large.ims /out/retina_large.ome.tiff
+
+# Large EM 
+/opt/bftools/bfconvert -no-upgrade -noflat -compression zlib -pyramid-resolutions 5 -pyramid-scale 2 -tilex 1024 -tiley 1024 BGal_000438_frames.dm4 /out/BGal_000438_frames.ome.tiff
